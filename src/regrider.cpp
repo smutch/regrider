81c7e02356e6eea14caf2fcc140073f3ebfa6738
Simon Mutch <smutch@unimelb.edu.au>
2019-12-10 07:31:54 +0000

    Read in gbpTrees grid

--- src/regrider.cpp
+++ src/regrider.cpp
@@ -21,10 +21,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstring>
 
 #include "../include/cxxopts.hpp"
 
-static void read_gbptrees(const std::string fname_in)
+
+static void read_gbptrees(const std::string fname_in, const std::string grid_name)
 {
     fmt::print("Reading gbpTrees grid file {}...\n", fname_in);
     std::ifstream ifs(fname_in, std::ios::binary | std::ios::in);
@@ -38,12 +40,43 @@ static void read_gbptrees(const std::string fname_in)
     fmt::print("box_size = {}\n", fmt::join(box_size, ","));
 
     int32_t n_grids;
-    ifs.read(reinterpret_cast<char*>(&n_grids), sizeof(int) * 3);
-    fmt::print("n_cell = {}\n", n_grids);
+    ifs.read(reinterpret_cast<char*>(&n_grids), sizeof(int));
+    fmt::print("n_grids = {}\n", n_grids);
 
     int32_t ma_scheme;
-    ifs.read(reinterpret_cast<char*>(&ma_scheme), sizeof(int) * 3);
-    fmt::print("n_cell = {}\n", ma_scheme);
+    ifs.read(reinterpret_cast<char*>(&ma_scheme), sizeof(int));
+    fmt::print("ma_scheme = {}\n", ma_scheme);
+
+    auto total_cells = n_cell[0] * n_cell[1] * n_cell[2];
+    std::vector<float> orig(total_cells, 0);
+
+    bool found = false;
+    for(int32_t ii=0; ii<n_grids && !found; ++ii){
+
+        std::string ident(32, '\0');
+        ifs.read(reinterpret_cast<char*>(ident.data()), sizeof(ident));
+        ident.resize(strlen(ident.c_str()));
+        
+        if (grid_name == ident) {
+            fmt::print("Reading grid {}...\n", ident);
+            ifs.read(reinterpret_cast<char*>(orig.data()), sizeof(float)*total_cells);
+            found = true;
+        } else {
+            fmt::print("Skipping grid {}...\n", ident);
+            ifs.seekg(sizeof(float)*total_cells, std::ifstream::cur);
+        }
+
+    }
+
+    if (!found) {
+        fmt::print(stderr, "Failed to find grid named `{}' in file!\n", grid_name);
+    }
+
+    // // DEBUG
+    // {
+    //     std::vector subset(orig.begin(), orig.begin()+10);
+    //     fmt::print("First 10 elements = {}\n", fmt::join(subset, ","));
+    // }
 
     ifs.close();
     fmt::print("...done\n");
@@ -57,6 +90,7 @@ int main(int argc, char* argv[])
         "regrider", "Downsample gbpTrees and VELOCIraptor trees using FFTW");
 
     options.add_options()("d,dim", "new grid dimension", cxxopts::value<uint32_t>())
+        ("n,name", "grid name (must match conventions), ", cxxopts::value<std::string>())
         ("g,gbptrees", "input gbpTrees grid file", cxxopts::value<std::string>())
         ("v,velociraptor", "input VELOCIraptor grid file", cxxopts::value<std::string>());
 
@@ -68,7 +102,7 @@ int main(int argc, char* argv[])
     }
 
     if (vm.count("gbptrees")) {
-        read_gbptrees(vm["gbptrees"].as<std::string>());
+        read_gbptrees(vm["gbptrees"].as<std::string>(), vm["name"].as<std::string>());
     }
 
     return 0;
