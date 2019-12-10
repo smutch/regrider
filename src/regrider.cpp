37cc3e43ab3ed58e1a5baf88027cadd0731c1223
Simon Mutch <smutch@unimelb.edu.au>
2019-12-10 05:59:53 +0000

    Format the code (WebKit style)

--- src/regrider.cpp
+++ src/regrider.cpp
@@ -1,17 +1,17 @@
 /*
  * regrider: Downsample gbpTrees and VELOCIraptor grids using FFTW
  * Copyright © 2019 Simon Mutch
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -19,12 +19,13 @@
 #include <fmt/core.h>
 #include <fmt/ostream.h>
 #include <fstream>
-#include <vector>
 #include <string>
+#include <vector>
 
 #include "../include/cxxopts.hpp"
 
-static void read_gbptrees(const std::string fname_in) {
+static void read_gbptrees(const std::string fname_in)
+{
     fmt::print("Reading gbpTrees grid file {}...\n", fname_in);
     std::ifstream ifs(fname_in, std::ios::binary | std::ios::in);
 
@@ -48,17 +49,14 @@ static void read_gbptrees(const std::string fname_in) {
     fmt::print("...done\n");
 }
 
-static void write_gbptrees(const std::string fname_out) {
-    
-}
+static void write_gbptrees(const std::string fname_out) {}
 
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    cxxopts::Options options("regrider", "Downsample gbpTrees and VELOCIraptor trees using FFTW");
+    cxxopts::Options options(
+        "regrider", "Downsample gbpTrees and VELOCIraptor trees using FFTW");
 
-    options.add_options()
-        ("d,dim", "new grid dimension", cxxopts::value<uint32_t>())
+    options.add_options()("d,dim", "new grid dimension", cxxopts::value<uint32_t>())
         ("g,gbptrees", "input gbpTrees grid file", cxxopts::value<std::string>())
         ("v,velociraptor", "input VELOCIraptor grid file", cxxopts::value<std::string>());
 
