711c2e593e9989db4f5bbffc6e3b3c08f4ffa689
Simon Mutch <smutch@unimelb.edu.au>
2021-08-03 16:46:37 +1000

    Fix up bootstrap-dev.sh script

--- bootstrap-dev.sh
+++ bootstrap-dev.sh
@@ -1,15 +1,18 @@
-echo "If you haven't already, please be sure and run\n\tspack compiler find && spack external find\n"
+echo "If you haven't already, please be sure and run"
+echo "  spack compiler find && spack external find"
+echo
 
 while true; do
-    read -p "Do you wish to setup the spack env with all requirements?" yn
+    read -p "Do you wish to setup the spack env with all requirements? [Y/n] " yn
     case $yn in
-        [Yy]* ) make install; break;;
+        [Yy]* ) break;;
+        ''    ) break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
 spack env create -d ./spack-env spack.yaml
-spack env activate ./spack-env
+eval `spack env activate --sh ./spack-env`
 spack concretize
 spack install
